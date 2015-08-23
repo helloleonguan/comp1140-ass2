5a1656229d2bd9ccc263986ef2e8bb494194a4df
Jack <jadamson08@gmail.com>
2015-08-23 14:19:23 +1000

    Created Tiles class.

---
+++ src/comp1140/ass2/Tiles.java
@@ -0,0 +1,12 @@
+package comp1140.ass2;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Jack on 23/08/2015.
+ */
+public class Tiles {
+    static ArrayList<Point>[] Pieces = new ArrayList[21];
+    
+}
